9bbefa83c89981fb91174c419a830147a7fbb7ea
Amit Lap <amitlap@edu.hac.ac.il>
2024-05-12 17:36:55 +0300

    Importing components and managing app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EntranceScreen from './components/EntranceScreen';
+import GameScreen from './components/GameScreen';
+import LeaderboardModal from './components/LeaderboardModal';
+import GameSettingsModal from './components/GameSettingsModal';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<EntranceScreen />} />
+                    <Route path="/game" element={<GameScreen />} />
+                    <Route path="/leaderboard" element={<LeaderboardModal />} />
+                    <Route path="/settings" element={<GameSettingsModal />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
