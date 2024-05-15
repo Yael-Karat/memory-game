16ad09090e9a18ebbc1498dadb00593c4d568022
Amit Lap <amitlap@edu.hac.ac.il>
2024-05-15 14:44:25 +0300

    Updated the code accordingly to the other parts of the project.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /**import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';**/
+import App from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';**/
 
 /**ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +15,7 @@ import reportWebVitals from './reportWebVitals';
   document.getElementById('root')
 );**/
 
-createRoot(document.getElementById('root')).render(
+/**createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
@@ -24,4 +24,17 @@ createRoot(document.getElementById('root')).render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();**/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Router>
+        <App />
+    </Router>
+);
