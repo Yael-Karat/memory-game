c04382f473cb354d9f5fd2c0850c76d1933db065
Amit Lap <amitlap@edu.hac.ac.il>
2024-05-15 14:42:59 +0300

    Updated the code accordingly to the other parts of the project.

--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -3,10 +3,23 @@
  * @param {Array} array - Array to be shuffled.
  * @returns {Array} - Shuffled array.
  */
-export function shuffle(array) {
+/**export function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-}
+}**/
+
+export const shuffle = (array) => {
+    let currentIndex = array.length, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+};
+
